232fa199446bfd59dea5f23d28a82bd9d4be47ee
arinabilan <arina.bilan@usach.cl>
2024-10-05 19:29:45 -0300

    Se crea el proyecto tingeso

---
+++ src/test/java/bank/app/appbank/AppBankApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.app.appbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
